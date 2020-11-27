b2bac5b37358c85e569c2e4593b515b263462c00
Patricia <p.santamartamedina@gmail.com>
2020-11-27 14:05:15 +0100

    0497.04 añadimos un metodo que muestra las estadisticas y se incluyen parametros que indica a la temperatura máxima y minima que se ha puesto la maquina

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -5,12 +5,20 @@ public class AireAcondicionado{
     private double min;
     private double max;
     private double incremento;
+    private int contador;
+    private double minSeleccionada;
+    private double maxSeleccionada;
+    private double temperaturaActual;
 
     public AireAcondicionado(double minimo, double maximo){
         temperatura = 15.0;
         min = minimo;
         max = maximo;
         incremento = 5.0;
+        contador = 0;
+        minSeleccionada = temperatura;
+        maxSeleccionada = temperatura;
+        temperaturaActual = temperatura;
     }
 
     public double getTemperatura(){
@@ -22,16 +30,30 @@ public class AireAcondicionado{
     }
     
     public void subirTemperatura(){   
-        if (temperatura + incremento <= max ){
+       if (temperatura + incremento <= max ){
             temperatura = temperatura + incremento;
+            temperaturaActual = temperatura;
+            contador ++;
        }
+       if (temperaturaActual > maxSeleccionada){
+           maxSeleccionada = temperaturaActual;
+       }
+       
     }
     
     public void bajarTemperatura(){ 
         if (temperatura - incremento >= min ){
             temperatura = temperatura - incremento;
+            temperaturaActual = temperatura;
+            contador ++;
+       }
+       if (temperaturaActual < minSeleccionada){
+           minSeleccionada = temperaturaActual;
        }
     }
     
+    public void mostrarEstadisticas(){
+        System.out.println("Temperatura actual: " + temperatura + "º, Temp. máxima posible: " + max + "º, Temp. mínima posible: " + min + "º, Temp. mínima seleccionada: " + minSeleccionada + "º, Temp. máxima seleccionada: " + maxSeleccionada + "º, Temp. cambiada " + contador + " veces");
+    }
     
 }
