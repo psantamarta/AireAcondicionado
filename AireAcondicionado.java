b9a7c1086b070e6d68aff677e29eb149afda7131
Patricia <p.santamartamedina@gmail.com>
2020-11-27 13:24:23 +0100

    0497.03 se incluyen parametros para max y min de temperatura y solo puede subirla o bajarla en caso de que no lo sobrepase. tambien se incluye metodo setter para la temperatura

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -2,22 +2,35 @@
 public class AireAcondicionado{
     
     private double temperatura;
+    private double min;
+    private double max;
+    private double incremento;
 
-
-    public AireAcondicionado(){
+    public AireAcondicionado(double minimo, double maximo){
         temperatura = 15.0;
+        min = minimo;
+        max = maximo;
+        incremento = 5.0;
     }
 
     public double getTemperatura(){
         return temperatura;
     }
     
-    public void subirTemperatura(){        
-        temperatura += 5;
+    public void setTemperatura(double nuevaTemperatura){
+        temperatura =  nuevaTemperatura;
     }
     
-    public void bajarTemperatura(){        
-        temperatura -= 5;
+    public void subirTemperatura(){   
+        if (temperatura + incremento <= max ){
+        temperatura = temperatura + incremento;
+       }
+    }
+    
+    public void bajarTemperatura(){ 
+        if (temperatura - incremento >= min ){
+        temperatura = temperatura - incremento;
+       }
     }
     
     
